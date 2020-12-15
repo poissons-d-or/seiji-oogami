e116b286f1e852c94d461c17a6f25c38d7c8c212
ogami <ijies.imago@gmail.com>
2020-12-16 00:21:45 +0900

    Add: 投稿画面の表示改変、および投稿画面におけるいいね、リツイート機能実装

--- php-rt-iine/html/php-challenge/view.php
+++ php-rt-iine/html/php-challenge/view.php
@@ -2,14 +2,115 @@
 session_start();
 require('dbconnect.php');
 
+if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
+  // ログインしている
+  $_SESSION['time'] = time();
+
+  $members = $db->prepare('SELECT * FROM members WHERE id=?');
+  $members->execute([$_SESSION['id']]);
+  $member = $members->fetch();
+} else {
+  // ログインしていない
+  header('Location: login.php');
+  exit();
+}
+
 if (empty($_REQUEST['id'])) {
   header('Location: index.php');
   exit();
 }
 
-// 投稿を取得する
-$posts = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id AND p.id=? ORDER BY p.created DESC');
+// 投稿を取得する *****課題で改変*****
+// displayテーブルとリレーションを張る
+$posts = $db->prepare(
+  'SELECT p.*, m.name, m.picture, d.* 
+   FROM members m, posts p
+   LEFT JOIN display d ON p.id=d.post_id
+   WHERE m.id=p.member_id AND d.post_id=? 
+   ORDER BY d.display_created DESC'
+);
 $posts->execute([$_REQUEST['id']]);
+
+// *****ここから課題で追加***** 
+// ログインユーザーがいいねした投稿を取得する
+$likes = $db->prepare(
+  'SELECT liked_post_id
+   FROM likes
+   WHERE member_id=?'
+);
+$likes->execute([$member['id']]);
+while ($likedPost = $likes->fetch()) {
+  $myLikes[] = $likedPost['liked_post_id'];
+}
+// ログインユーザーがリツイートした投稿を取得する
+$retweets = $db->prepare(
+  'SELECT post_id
+   FROM display
+   WHERE display_member_id=? AND is_retweet=TRUE'
+);
+$retweets->execute([$member['id']]);
+while ($RTedPost = $retweets->fetch()) {
+  $myRTs[] = $RTedPost['post_id'];
+}
+// いいねボタンクリック時の処理
+if (isset($_REQUEST['like'])) {
+  if (isset($myLikes) && in_array($_REQUEST['like'], $myLikes)) { // いいね済みの投稿に対する処理
+    $cancel = $db->prepare(
+      'DELETE FROM likes
+       WHERE liked_post_id=? AND member_id=?'
+    );
+    $cancel->execute([
+      $_REQUEST['like'],
+      $member['id']
+    ]);
+    // いいね取消し後、投稿画面へ戻る
+    header('Location: view.php?id=' . $_REQUEST['like']);
+    exit();
+  } else { // いいねしていない投稿に対する処理
+    $liked = $db->prepare(
+      'INSERT INTO likes
+       SET liked_post_id=?, member_id=?, created_at=NOW()'
+    );
+    $liked->execute([
+      $_REQUEST['like'],
+      $member['id']
+    ]);
+    // いいねの処理を実行し、投稿画面へ戻る
+    header('Location: view.php?id=' . $_REQUEST['like']);
+    exit();
+  }
+}
+//リツイートボタンクリック時の処理
+if (isset($_REQUEST['rt'])) {
+  if (isset($myRTs) && in_array($_REQUEST['rt'], $myRTs)) {
+    // リツイート済の投稿に対する処理
+    $rtCancel = $db->prepare(
+      'DELETE FROM display
+       WHERE post_id=? AND display_member_id=? AND is_retweet=TRUE'
+    );
+    $rtCancel->execute([
+      $_REQUEST['rt'],
+      $member['id']
+    ]);
+    // リツイート取消し後、投稿画面へ戻る
+    header('Location: view.php?id=' . $_REQUEST['rt']);
+    exit();
+  } else {
+    // リツイートしていない投稿に対する処理
+    $retweeted = $db->prepare(
+      'INSERT INTO display
+       SET post_id=?, display_member_id=?, is_retweet=TRUE, display_created=NOW();'
+    );
+    $retweeted->execute([
+      $_REQUEST['rt'],
+      $member['id']
+    ]);
+    // リツイートの処理を実行し、投稿画面へ戻る
+    header('Location: view.php?id=' . $_REQUEST['rt']);
+    exit();
+  }
+}
+// *****ここまで課題で追加*****
 
 // htmlspecialcharsのショートカット
 function h($value)
@@ -45,9 +146,86 @@ function makeLink($value)
       <p>&laquo;<a href="index.php">一覧にもどる</a></p>
       <?php if ($post = $posts->fetch()) : ?>
         <div class="msg">
+          <!-- *****名前、投稿、日時、返信ボタンの表示を改変***** -->
           <img src="member_picture/<?php echo h($post['picture']); ?>" width="60" alt="<?php echo h($post['name']); ?>" />
-          <p><?php echo makeLink(h($post['message'])); ?><span class="name">(<?php echo h($post['name']); ?>)</span></p>
-          <p class="day"><?php echo h($post['created']); ?></p>
+          <p>
+            <span class="name"><?php echo h($post['name']); ?></span>
+            <span class="day">
+              <?php echo h($post['created']); ?>
+              <?php if ($post['reply_post_id'] > 0) : ?>
+                <a href="view.php?id=<?php echo h($post['reply_post_id']); ?>">返信元のメッセージ</a>
+              <?php endif; ?>
+              <?php if ($_SESSION['id'] === $post['member_id']) : ?>
+                [<a href="delete.php?id=<?php echo h($post['post_id']); ?>" style="color: #F33;">削除</a>]
+              <?php endif; ?>
+            </span><!-- /.day -->
+            <br>
+            <?php echo makeLink(h($post['message'])); ?>
+          </p>
+
+          <!-- *****ここから課題で追加***** -->
+          <?php
+          // いいね数の取得
+          $likeCounts = $db->prepare(
+            'SELECT COUNT(*) AS likeCNT
+             FROM likes
+             WHERE liked_post_id=?'
+          );
+          $likeCounts->execute([$post['post_id']]);
+          $likeCount = $likeCounts->fetch();
+          // リツイート数の取得
+          $rtCounts = $db->prepare(
+            'SELECT COUNT(*) AS rtCNT
+             FROM display
+             WHERE post_id=? AND is_retweet=TRUE'
+          );
+          $rtCounts->execute([$post['post_id']]);
+          $rtCount = $rtCounts->fetch();
+          ?>
+          <div class="icons">
+            <a href="index.php?res=<?php echo h($post['post_id']); ?>"><img src="images/respond.png" alt="返信する"></a>
+
+            <a class="like-button" href="view.php?id=<?php echo h($post['post_id']); ?>&like=<?php echo h($post['post_id']); ?>">
+              <?php // ログイン中のユーザーがいいねした投稿のidをチェック
+              if (isset($myLikes)) {
+                if (in_array($post['post_id'], $myLikes)) {
+              ?>
+                  <!--  ログイン中のユーザーがいいねしている場合 -->
+                  <img src="images/liked.png" alt="いいねを取消す">
+                <?php
+                } else {
+                ?>
+                  <!--  ログイン中のユーザーがいいねしていない場合 -->
+                  <img src="images/like.png" alt="いいねする">
+              <?php
+                }
+              }
+              ?>
+              <!-- いいねされた数を表示 -->
+              <?php echo $likeCount['likeCNT']; ?>
+            </a><!-- /.like-button -->
+
+            <a class="retweet-button" href="view.php?id=<?php echo h($post['post_id']); ?>&rt=<?php echo h($post['post_id']); ?>">
+              <?php // ログイン中のユーザーがリツイートした投稿のidをチェック
+              if (isset($myRTs)) {
+                if (in_array($post['post_id'], $myRTs)) {
+              ?>
+                  <!-- ログイン中のユーザーがリツイートしている場合 -->
+                  <img src="images/retweeted.png" alt="リツイートを取消す">
+                <?php
+                } else {
+                ?>
+                  <!-- ログイン中のユーザーがリツイートしていない場合 -->
+                  <img src="images/retweet.png" alt="リツイートする">
+              <?php
+                }
+              }
+              ?>
+              <!-- リツイートされた数を表示 -->
+              <?php echo $rtCount['rtCNT']; ?>
+            </a><!-- /.retweet-button -->
+          </div><!-- /.icons -->
+          <!-- *****ここまで課題で追加**** -->
         </div>
       <?php else : ?>
         <p>その投稿は削除されたか、URLが間違っています</p>
