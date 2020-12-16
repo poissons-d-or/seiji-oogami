7d3fb85236f0ef7754d1933929a5b82aa198f7ae
ogami <ijies.imago@gmail.com>
2020-12-16 12:11:08 +0900

    Add: 練習問題1を回答

--- php-rt-iine/html/php-pre-challenge1/index.php
+++ php-rt-iine/html/php-pre-challenge1/index.php
@@ -1,12 +1,12 @@
 <?php
 for ($i = 1; $i <= 100; $i++) {
-    if ($i % 3 == 0 && $i % 5 == 0) {
+    if ($i % 3 === 0 && $i % 5 === 0) {
         echo "3の倍数であり、5の倍数";
         echo "<br>";
-    } elseif ($i % 3 == 0) {
+    } elseif ($i % 3 === 0) {
         echo "3の倍数";
         echo "<br>";
-    } elseif ($i % 5 == 0) {
+    } elseif ($i % 5 === 0) {
         echo "5の倍数";
         echo "<br>";
     } else {
